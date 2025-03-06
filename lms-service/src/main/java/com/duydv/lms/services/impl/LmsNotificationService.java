8b012fc52bdec89b0a3d4177b32960c2c7b57aba
duydv <duydv>
2025-03-06 10:24:54 +0700

    Make mvnw executable

--- lms-service/src/main/java/com/duydv/lms/services/impl/LmsNotificationService.java
+++ lms-service/src/main/java/com/duydv/lms/services/impl/LmsNotificationService.java
@@ -1,4 +1,10 @@
 package com.duydv.lms.services.impl;
 
-public class LmsNotificationService {
+import com.duydv.lms.entities.LmsNotification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface LmsNotificationService {
+
+  Page<LmsNotification> findAllPageable(Pageable pageable);
 }
--- lms-service/src/main/java/com/duydv/lms/services/impl/LmsNotificationServiceImpl.java
+++ lms-service/src/main/java/com/duydv/lms/services/impl/LmsNotificationServiceImpl.java
@@ -1,4 +1,22 @@
 package com.duydv.lms.services.impl;
 
-public class LmsNotificationServiceImpl {
+import com.duydv.lms.entities.LmsNotification;
+import com.duydv.lms.repositories.LmsNotificationRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LmsNotificationServiceImpl implements LmsNotificationService {
+
+  private LmsNotificationRepository lmsNotificationRepository;
+
+  @Override
+  public Page<LmsNotification> findAllPageable(Pageable pageable) {
+    return lmsNotificationRepository.findAll(pageable);
+  }
 }
